4bfdbea67fcca2c453cc662bb1d2194af6afa72e
Gokce Ozel <norm.developer@algomedi.com>
2021-03-22 15:51:22 +0300

    Create Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './compomemts/product/product.component';
+import { CategoryComponent } from './compomemts/category/category.component';
+import { NaviComponent } from './compomemts/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
