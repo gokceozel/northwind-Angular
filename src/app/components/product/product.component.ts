f803801a81c9bc29ba010cfa9ab19af4d1098521
Gokce Ozel <norm.developer@algomedi.com>
2021-04-22 17:15:48 +0300

    Add Cart Method

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 
@@ -20,7 +21,8 @@ export class ProductComponent implements OnInit {
 
   constructor(private productService:ProductService,
               private activatedRoute:ActivatedRoute,
-              private toastrService:ToastrService) { }
+              private toastrService:ToastrService,
+              private cartService:CartService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -47,8 +49,16 @@ export class ProductComponent implements OnInit {
   }
 
   addToCart(product:Product){
-    this.toastrService.success("Sepete Eklendi",product.productName)
-    console.log(product);
+
+    if(!product.productId){
+       this.toastrService.error("Hata","Bu ürün sepete eklenemez");
+     }
+    else{
+      this.toastrService.success("Sepete Eklendi",product.productName);
+      this.cartService.addToCart(product);
+      console.log(product);
+     }
+
   }
 
 }
