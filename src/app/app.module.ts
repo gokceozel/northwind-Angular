a604621dbbfe7c3338106c7e655ae271a381247d
Gokce Ozel <norm.developer@algomedi.com>
2021-04-22 15:35:13 +0300

    Change Folder Name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,20 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './compomemts/product/product.component';
-import { CategoryComponent } from './compomemts/category/category.component';
-import { NaviComponent } from './compomemts/navi/navi.component';
+
 import { from } from 'rxjs';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 import {ToastrModule} from "ngx-toastr";
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
     CategoryComponent,
+    ProductComponent,
     NaviComponent,
     VatAddedPipe,
     FilterPipePipe
